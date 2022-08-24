cc89f77331c49a30b9ecaf9434973b7bffd85085
Farzaneh Mirzakhanloo <farzaneh.mirzakhanloo@gmail.com>
2022-08-24 21:15:07 +0430

    add type files

---
+++ src/screens/home/components/UserItem.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { Image, Text } from '../../../shared/components';
+import { responsiveHeight as rh, responsiveWidth as rw } from "react-native-responsive-dimensions";
+import { User } from '../../../shared/types';
+
+const UserItem = (user: User) => {
+    return (
+        <Pressable style={styles.container}>
+            <Image source={{ uri: "" }} style={styles.userImage} />
+            <View style={styles.nameContainer}>
+                <Text bold>{user.username}</Text>
+                <Text>{user.name}</Text>
+            </View>
+        </Pressable>
+    )
+}
+
+export default UserItem;
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+    }, userImage: {
+        width: rw(20),
+        height: rw(20),
+    }, nameContainer: {
+        flexDirection: 'row'
+    }
+})
---
+++ src/shared/types/index.ts
@@ -0,0 +1,11 @@
+import User from "./User";
+import Comment from "./Comment";
+import Album from "./Album";
+import Photo from "./Photo";
+
+export {
+    User,
+    Comment,
+    Album,
+    Photo,
+}
